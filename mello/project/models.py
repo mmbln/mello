587b3f48544934e06e0be1519dcc400da15463b7
Matthias Mueller <mmblnfrn@gmail.com>
2015-07-27 12:03:07 +0200

    First commit after working template constuct and first migration
    Project has its first model definition

--- mello/project/models.py
+++ mello/project/models.py
@@ -1,3 +1,12 @@
+# project/models.py
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length = 100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    
