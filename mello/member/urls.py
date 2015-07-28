6dc5954e582ae6093b3744d9d34c2260bb75c7ce
Matthias Mueller <mmblnfrn@gmail.com>
2015-07-28 18:16:37 +0200

    first url in member
    url of member used in project view

--- mello/member/urls.py
+++ mello/member/urls.py
@@ -17,8 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'project.views.home', name='mello_home'),
-    url(r'^project/', include('project.urls')),
-    url(r'^member/', include('member.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'member.views.view_member_list', name='member_list'),
+#    url(r'^(?P<id>\d+)/$', 'project.views.view_project', name='project_view'),
+#    url(r'^new/$', 'project.views.new_project', name='project_new'),
 ]
--- mello/member/views.py
+++ mello/member/views.py
@@ -1,3 +1,11 @@
+# member/views
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+def view_member_list(request):
+    return HttpResponse('member_view')
