a8002471f428fef72cf67e7110bac0ec5c136b27
Matthias Mueller <mmblnfrn@gmail.com>
2015-08-07 12:15:21 +0200

    Validation added to Member form
    email and name must be unique

--- mello/member/forms.py
+++ mello/member/forms.py
@@ -3,8 +3,28 @@
 
 from django import forms
 
+from .models import Member
+
 class MemberForm(forms.Form):
     login_name = forms.CharField(max_length=100,required=True)
     email = forms.EmailField(max_length=1000)
     admin = forms.BooleanField()
-    
+
+    def clean_login_name(self):
+        ln = self.cleaned_data.get('login_name')
+        try:
+            Member.objects.get(login_name=ln)
+        except:
+            return ln
+        raise forms.ValidationError('Login Name already exists')
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            Member.objects.get(email=email)
+        except:
+            return email
+        raise forms.ValidationError('Email Address already exists')
+
+
--- mello/member/views.py
+++ mello/member/views.py
@@ -6,6 +6,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from .forms import MemberForm
+from .models import Member
+
+#----------------------------------------------------------------
+# utils
+#----------------------------------------------------------------
+
+def generate_password():
+    # TODO use random generator
+    return ('123')
+
+#----------------------------------------------------------------
+# views
+#----------------------------------------------------------------
 
 def view_member_list(request):
     return render(request, 'member_list.html')
@@ -13,7 +26,25 @@ def view_member_list(request):
 def add_member(request):
     if request.method == 'POST':
         form = MemberForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print('Form is valid')
+            # All data entered correctly, save the data
+            temp_password = generate_password()
+            login_name = form.cleaned_data['login_name']
+            email = form.cleaned_data['email']
+            if form.cleaned_data['admin']:
+                Member.objects.create_superuser(email=email,
+                                                login_name=login_name,
+                                                password=temp_password)
+            else:
+                Member.objects.create_user(email=email,
+                            login_name=login_name,
+                            password=temp_password)
+            # TODO send email to user
+            HttpResponseRedirect(reverse('member_list'))
+        else:
+            # Show form with errors
+            print('Form is not valid')
     else:
         form = MemberForm()
     
