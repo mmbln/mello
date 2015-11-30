36ac0ad71d7090f033a9ca3157f8e3d465771eab
Matthias Mueller <mmblnfrn@gmail.com>
2015-11-30 23:44:21 +0100

    got a little closer to the file upload solution

--- mello/static/js/new_picture.js
+++ mello/static/js/new_picture.js
@@ -8,11 +8,29 @@ function changePicture()
 {
     /* file dialog */
     /* user id */
+    console.log("changePicture");
+    
     $("#fileinput").click()
 }
 
 function setStandardPicture()
 {
     /* */
-    $("#id_image").attr("src", "/member/img/0")
+    console.log("setStandardPicture");
+
+    $("#id_image").attr("src", "/member/img/0");
+}
+
+function loadFile(evt)
+{
+    image=evt.target.files[0];
+    if (image.type.match("image/*")) {
+	var reader = new FileReader();
+	reader.onload = function(e) {
+	    // TODO bild verkleinern und speichern als png
+	    $("#id_image").attr("src", e.target.result);
+	};
+	reader.readAsDataURL(image);
+    }
+    console.log("loadFile");
 }
